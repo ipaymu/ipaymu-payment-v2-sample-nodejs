3a17f3312ba8ac0564fff0b27136ec7c69fd64ec
Edi <40590916+putuedipuspayasa@users.noreply.github.com>
2023-03-07 12:09:07 +0800

    add sample direct payment api

--- ipaymu_direct_payment.js
+++ ipaymu_direct_payment.js
@@ -1,29 +1,22 @@
-// required cryptojs untuk enkripsi
-// required node-fetch untuk request data
-// var CryptoJS        = require("crypto-js"); // npm install crypto-js
-// var fetch           = require('node-fetch'); // npm install node-fetch --save
-
 import fetch from 'node-fetch'; // npm install crypto-js
 import CryptoJS from 'crypto-js'; // npm install node-fetch --save
 
 // adjust with your iPaymu api key & va 
 var apikey          = "QbGcoO0Qds9sQFDmY0MWg1Tq.xtuh1";
 var va              = "1179000899";
-var url             = 'https://sandbox.ipaymu.com/api/v2/payment'; // development mode
-// var url             = 'https://my.ipaymu.com/api/v2/payment'; // for production mode
+var url             = 'https://sandbox.ipaymu.com/api/v2/payment/direct'; // development mode
+// var url             = 'https://my.ipaymu.com/api/v2/payment/direct'; // for production mode
 
 var body            = {
-    "product":["Jacket"],
-    "qty":["1"],
-    "price": ["150000"],
-    "amount":"10000",
-    "returnUrl":"https://your-website.com/thank-you-page", //your thank you page url
-    "cancelUrl":"https://your-website.com/cancel-page", // your cancel page url
+    "name":"Putu",
+    "phone":"08123456789",
+    "email": "putu@gmail.com",
+    "amount": 10000,
+    "comments":"Payment to XYZ Store",
     "notifyUrl":"https://your-website.com/callback-url", // your callback url
     "referenceId":"1234", // your reference id or transaction id
-    "buyerName":"Putu", // optional
-    "buyerPhone":"08123456789", // optional
-    "buyerEmail":"buyer@mail.com", // optional
+    "paymentMethod":"va",
+    "paymentChannel":"bca",
 } 
 // generate signature
 var bodyEncrypt     = CryptoJS.SHA256(JSON.stringify(body));
@@ -35,7 +28,8 @@ fetch(
     {
         method: "POST",
         headers: {
-            Accept: 'application/json', 'Content-Type': 'application/json',
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
             va: va,
             signature: signature,
             timestamp: '20150201121045'
